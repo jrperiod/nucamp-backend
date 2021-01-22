8f40868dbda630676ff3be8fcc225f7f1512a9aa
jrperiod <lbaileyjr@gmail.com>
2021-01-21 19:58:51 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: 'YOUR FACEBOOK APP ID HERE',
+        clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
+    }
 }
